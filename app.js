940c10eae380b6253afe8ef3385120c99e7533cf
Aryan Arora <aryanarora@Aryans-MacBook-Pro.local>
2020-07-15 01:16:00 +0800

    jsss

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
-const firebase = require("firebase");
-// Required for side-effects
-require("firebase/firestore");
+const texts = ['code', 'design', 'cook', 'watch movies', 'play games'];
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
 
-// Initialize Cloud Firestore through Firebase
-firebase.initializeApp({
-    apiKey: 'AIzaSyCnK4ClGsb2vn4An-JBSrFUrfQ0j49nR_c',
-    authDomain: 'resume-mp1-ccapdev.firebaseapp.com',
-    projectId: 'resume-mp1-ccapdev'
-  });
-  
-  var db = firebase.firestore();
+(function type () {
 
-  
+  if (count === texts.length){
+    count = 0;
+  }
+  currentText = texts[count];
+  letter = currentText.slice(0, ++index);
+
+  document.querySelector('.typing').textContent = letter;
+  if(letter.length === currentText.length){
+    count++;
+    index = 0;
+  }
+  setTimeout(type, 400);
+
+}());
