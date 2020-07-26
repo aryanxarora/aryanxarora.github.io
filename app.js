c295d968134121a8d84337df9aa5551f672743fa
Aryan Arora <aryanxarora@gmail.com>
2020-07-27 06:10:32 +0800

    mobile test

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const texts = ['code', 'design', 'cook', 'watch movies', 'play games'];
+const texts = ['design', 'code', 'cook', 'watch movies', 'play games'];
 let count = 0;
 let index = 0;
 let currentText = '';
@@ -20,3 +20,43 @@ let letter = '';
   setTimeout(type, 400);
 
 }());
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+        apiKey: "AIzaSyCnK4ClGsb2vn4An-JBSrFUrfQ0j49nR_c",
+        authDomain: "resume-mp1-ccapdev.firebaseapp.com",
+        databaseURL: "https://resume-mp1-ccapdev.firebaseio.com",
+        projectId: "resume-mp1-ccapdev",
+        storageBucket: "resume-mp1-ccapdev.appspot.com",
+        messagingSenderId: "822915553621",
+        appId: "1:822915553621:web:aa899a316eab401b5eb7dc",
+        measurementId: "G-0B2F0P89HS"
+};
+
+// Initialize Firebase
+var project = firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+var db = firebase.firestore()
+
+// ? THIS IS HOW YOU ADD DATA
+/*
+db.collection("other-stuff").doc().set({
+  text: "Something new"
+})
+.then(function(){
+  console.log("Success");
+})
+.catch(function (err){
+  console.log(err)
+});
+
+
+db.collection("other-stuff").get().then(function(snapshot){
+  snapshot.forEach(function(doc){
+      var otherStuff =  doc.data().text;
+      var temp = otherStuff;
+      console.log(temp);
+  })
+});
+
+*/
